fd40d8b5239d9f23d5a6d4395b0e240f42c7118a
Rushi <Rushi@LAPTOP-26H45Q5R>
2020-10-12 21:32:20 +0530

    qwergthj

---
+++ Practice/src/ArrayTwoConcatenate.java
@@ -0,0 +1,30 @@
+
+
+
+public class ArrayTwoConcatenate {
+	public static void main (String args[]) {
+	
+		
+		int a[][]= {{1,2,3,4},{5,6,7,8}};
+		
+		
+		
+		
+		for(int i=0; i<4; i++)
+		{
+			
+			for(int j=0; j<4; j++)
+			{
+				if(i==1||i==2||j)
+				System.out.print("*");
+			}
+				System.out.println(" ");
+		}
+					
+		System.out.println();
+	}
+		
+		
+	}
+	
+	
---
+++ Practice/src/StringAll.java
@@ -0,0 +1,48 @@
+
+public class StringAll {
+
+		  private static final char[] String = null;
+
+		public static void main(String[] args) {
+			//String Length
+		    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		    //String Length
+		    System.out.println("The length of the txt string is: " + txt.length());
+		    
+		    // toUpperCase() and toLowerCase()
+		    System.out.println(txt.toUpperCase());
+		    System.out.println(txt.toLowerCase());
+		    
+		    //Finding a Character in a String
+		    String txt1 = "Please locate where 'locate' occurs!";
+		    System.out.println(txt1.indexOf("s"));
+		    
+		    //String Concatenation
+		    String firstName = "Rushi";
+		    String lastName = "Chandile";
+		    System.out.println(firstName + " " + lastName);
+		  
+		    //Special Characters
+		    
+		    String txt11 = "The character \\ is called backslash.";
+		    System.out.println(txt11);
+		    String txt12 = "It\'s alright.";
+		    System.out.println(txt12);
+		    String txt13 = "We are the so-called \"Vikings\" from the north.";
+		    System.out.println(txt13);
+		    
+		    //string addition
+		    int x = 103;
+		    int y = 203;
+		    int z = x + y; 
+		    System.out.println (z);
+		    
+		  }
+		    
+    
+		  
+		}
+
+
+
+
